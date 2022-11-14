66e949b5eddf6e6d9a0c19b49cb545ee775e2c6d
arpitkarnak <arpitkarnak@users.noreply.github.com>
2022-11-14 23:49:22 +0530

    update : error handling if value <=0

--- area.js
+++ area.js
@@ -5,15 +5,30 @@ var output1 = document.querySelector('#output');
 
 
 function findarea(a,b){
+    if(a>=0,b>=0){
     const baseHeight = (a*b)/2
-    return baseHeight
+    return baseHeight}
+    else {
+        output1.innerText = "You need to provide value more than 0"
+        
+
+    }
     
 }
 
 
 function final(){
     const finalValue = findarea(Number(inputValue[0].value),Number(inputValue[1].value))
-    output1.innerText = "The Area of Triangle is " + finalValue 
+
+    if(finalValue<=0){
+        output1.innerText = "Please Provide Value more than 0 "  
+    } else if(finalValue=" ") {
+        output1.innerText = "Provide value more than 0"
+        
+    } else {
+        output1.innerText = "The Area of Triangle is " + finalValue 
+    }
+    
 }
 
 
