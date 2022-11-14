f4fdabbec10e65d4cf0bdaaa5f85097ba88a1492
arpitkarnak <arpitkarnak@users.noreply.github.com>
2022-11-15 01:16:14 +0530

    update : negative case

--- hypo.js
+++ hypo.js
@@ -3,10 +3,39 @@ var hypotenuseBtn = document.querySelector('#hypotenuse-btn');
 var output1 = document.querySelector('#output')
 
 
-function calculateSumOfSquares(a, b) {
+function calculateSumOfSquares(a,b){
+    
+    if(b>0){
 
-    const sumOfSquares = a * a + b * b;
-    return sumOfSquares;
+        if(a>0){
+
+    const sumOfSquares = a * a + b * b
+    
+    return sumOfSquares}
+
+    else {
+        output1.innerText = "Please Provide Value more than 0" 
+        return }
+
+    }
+    else if(a>0){
+
+        if(b>0){
+
+    const sumOfSquares = a * a + b * b
+   
+    return sumOfSquares}
+
+    else {
+        output1.innerText = "Please Provide Value more than 0" 
+        return }
+
+    }
+    
+    else {
+        output1.innerText = "Please Provide Value more than 0" 
+        return
+    }
 
 }
 
@@ -14,9 +43,23 @@ function calculateSumOfSquares(a, b) {
 
 
 function calculateHypotenuse() {
-    const sumOfSquares = calculateSumOfSquares(Number(sideInput[0].value), Number(sideInput[1].value));
-    const lengthofhypo = Math.sqrt(sumOfSquares)
-    output1.innerText = "The Length of Hypotenuse : " + lengthofhypo;
+    const sumOfSquare = calculateSumOfSquares(Number(sideInput[0].value),Number(sideInput[1].value));
+    const lengthofhypo = Math.sqrt(sumOfSquare)
+
+    if(lengthofhypo<=0){
+        output1.innerText = "Please Provide Value more than 0 " 
+        
+    } else if(lengthofhypo>0) {
+        output1.innerText = "The Length of Hypotenuse :" + lengthofhypo; 
+        
+    } else {
+        
+        output1.innerText = "Please Provide Value more than 0"
+    }
+
+
+
+    // output1.innerText = "The Length of Hypotenuse : " + lengthofhypo;
 
 }
 
